c32811fa1535769a0b1d8d2d25f687a4fbbc8755
salasberryfin <carlos.salas@suse.com>
2023-05-18 09:18:39 +0200

    Edit colorscheme

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,4 +1,4 @@
-vim.cmd.colorscheme("rasmus")
+vim.cmd.colorscheme("nord")
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
