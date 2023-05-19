0eade262e4e37ae4e549b84c31b118d384b88185
Carlos Salas <carlos.salas@suse.com>
2023-05-19 19:19:36 +0200

    add more tools to installer scripts

--- scripts/1_installs.sh
+++ scripts/1_installs.sh
@@ -3,7 +3,10 @@
 echo "Updating and installing packages with Zypper..."
 sudo zypper ref
 sudo zypper in -y neovim i3 tmux zsh helm gcc gcc-c++ npm i3status openconnect \
-    opi scrot arandr ripgrep
+    opi scrot arandr ripgrep aws-cli libffi-devel python3-devel libopenssl-devel \
+    ack docker flameshot
+# Azure cli from the SUSE repos fails -> install with Microsoft's script
+curl -L https://aka.ms/InstallAzureCli | bash
 
 # Go
 echo "Installing Go and related tools..."
@@ -11,11 +14,15 @@ sudo zypper in -y go
 PATH=$PATH:/home/$USER/go/bin
 go install golang.org/x/tools/gopls
 
-# Kubectl
+# kubernets
 echo "Installing kubernetes stuff: kubectl, helm, k3s, k3d..."
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
+echo "Installing k3s"
+curl -sfL https://get.k3s.io | sh -
+echo "Installing k3d"
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 # Configure Zsh
 echo "Installing oh-my-zsh..."
