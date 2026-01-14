1218d79f056fb254b7ea78946cc01e76231ac2a0
Carlos Salas <carlos.salas@suse.com>
2026-01-14 09:07:41 +0100

    disable nvim inlay_hints

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,5 +1,14 @@
+-- Explicitly disable stuff I don't like
 return {
-	-- Explicitly disabled plugins
+	-- Disable plugins
 	{ "folke/flash.nvim", enabled = false },
 	{ "hrsh7th/nvim-cmp", enabled = false },
+
+	-- Disable inlay_hints
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			inlay_hints = { enabled = false },
+		},
+	},
 }
